ba095a590bf5bb4f1cd7582d87539345fa456e2c
MadinaB <madinabektayeva@gmail.com>
2020-03-05 18:02:30 +0600

    added button

--- pokedex/src/App.js
+++ pokedex/src/App.js
@@ -4,12 +4,17 @@ import './App.css';
 
 function App() {
   const pokemon = ["Daniel", "Mike", "Nursultan"]
+
+  console.log(pokemon)
   return (
     <div className="App">
       <div className="pokedex">
 	<div className="pokedex-list">
 
-	  {pokemon.map(name => <li>000 {name}</li>)}
+	  {pokemon.map(name => <li key={name}>
+		<button>000 {name}</button>
+	  </li>)}
+
 	</div>
       </div>
     </div>
